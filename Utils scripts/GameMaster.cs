bf7818fbcd4f7dfe7f70ca09c8412dd81d03be19
Askodan <marcinkarczewski@gmail.com>
2017-02-02 16:35:16 +0100

    przeciwnicy i strzelanie

--- Utils scripts/GameMaster.cs
+++ Utils scripts/GameMaster.cs
@@ -6,4 +6,12 @@ public class GameMaster : Singleton<GameMaster> {
 
 	public GameObject [] players;
 	public GameObject [] hordes;
+
+	public int pointsForBobieslaw;
+	public int pointsForPtoszek;
+	public int pointsForPlayer;
+	public int pointsForSecond;
+	public int pointsForSmile;
+
+
 }
---
+++ Weapons/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	[HideInInspector]
+	public PlayerResults playerRes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Ptoszek") {
+			playerRes.ptoszekShoted++;
+			playerRes.points+=GameMaster.Instance.pointsForPtoszek;
+		}
+		if (col.tag == "Bobieslaw") {
+			playerRes.bobieslawShoted++;
+			playerRes.points+=GameMaster.Instance.pointsForBobieslaw;
+		}
+		if (col.tag == "Player") {
+			playerRes.playersShoted++;
+			playerRes.points+=GameMaster.Instance.pointsForPlayer;
+
+		}
+
+	}
+}
